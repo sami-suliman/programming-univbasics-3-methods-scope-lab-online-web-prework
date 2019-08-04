5cc969f09305496bf82f7869cb0ba07fa348b3d8
sami suliman <sami.bco@gmail.com>
2019-08-04 14:21:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -12,8 +12,6 @@ def link
   puts "It's Dangerous To Go Alone! Take This."
 end 
 
-def any_phrase(phrase)
+def any_phrase(phrase = "Do A Barrel Roll!")
   puts #{phrase} 
 end 
-
-any_phrase("Do A Barrel Roll!")
